c1c3148d89470a5b7543183123f8c364cc94baa3
Fajar Maftuh Fadli <fajarmaftuhfadli@gmail.com>
2023-03-15 08:01:30 +0700

    remove the need of credential and ignore tfstate

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "google" {
-  project     = var.project_id
-  region      = "us-central1"
-  credentials = file("./terraform.json")
+  project = var.project_id
+  region  = "us-central1"
+  # credentials = file("./terraform.json")
 }
 
 terraform {
